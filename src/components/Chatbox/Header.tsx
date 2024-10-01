812f6dee44747885f9a17f342569ac6cf05f43ab
Sean Bunch <databaws@gmail.com>
2024-10-01 09:55:18 -0400

    wavy header UI updates

--- src/components/Chatbox/Header.tsx
+++ src/components/Chatbox/Header.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions  } from 'react-native';
 import Svg, { Path } from 'react-native-svg';
 import BotInfo from './BotInfo';
+import WavyHeader from './WavyHeader';
 
 type HeaderProps = {
   name: string;
   navigation?: any;
   customStyles?: any;
 };
+
 // ===========================================================================
 // ===========================================================================
 // ===========================================================================
@@ -20,36 +22,50 @@ type HeaderProps = {
 const Header: React.FC<HeaderProps> = ({ navigation, name, customStyles }) => {
   // Your header component code here
   return (
-    <View style={customStyles}>
-
-     <View style={{ flexDirection: 'row', alignItems: 'center', backgroundColor: '#5000ca', height: 160 }}>
-     <Svg
-          height="60%"
-          width="100%"
-          viewBox="0 0 1440 320"
-          style={{ position: 'absolute', top: 130 }}
-        >
-          <Path
-            fill="#5000ca"
-            d="M0,96L48,112C96,128,192,160,288,186.7C384,213,480,235,576,213.3C672,192,768,128,864,128C960,128,1056,192,1152,208C1248,224,1344,192,1392,176L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
-          />
-        </Svg>
-      
+    <View style={styles.container}>
+      <WavyHeader customStyles={styles.svgCurve} />
+      <View style={styles.headerContainer}>
       <BotInfo name="My Bot" description="This is my bot"/>
-      
-     </View>
+
+
+      </View>
     </View>
   );
 };
 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  headerContainer: {
+    marginTop: 50,
+    marginHorizontal: 10
+  },
+  headerText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#333',
+    textAlign: 'center',
+    marginTop: 35
+  },
+  svgCurve: {
+    position: 'absolute',
+    width: Dimensions.get('window').width
+  },
+});
+
+
+
 // const Header = ({ navigation }: { navigation: any }) => {
-//   return (
-//     <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-//       <TouchableOpacity onPress={() => navigation.goBack()}>
-//         <Text>Back</Text>
-//       </TouchableOpacity>
-//       <Text style={{ marginLeft: 10 }}>Header</Text>
-//     </View>
+  //   return (
+    //     <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+    //       <TouchableOpacity onPress={() => navigation.goBack()}>
+    //         <Text>Back</Text>
+    //       </TouchableOpacity>
+    //       <Text style={{ marginLeft: 10 }}>Header</Text>
+    //     </View>
 //   );
 // };
 
